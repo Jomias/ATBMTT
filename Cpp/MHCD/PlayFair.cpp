c667d56d105ddfa3d64d0a58ab4e53720bab10f3
ADMIN <tt2324564@gmail.com>
2023-03-15 02:08:41 +0700

    add decrypt

--- Cpp/MHCD/PlayFair.cpp
+++ Cpp/MHCD/PlayFair.cpp
@@ -12,7 +12,7 @@ string remove_duplicate_string(string K) {
     }
     return ans;
 }
-string play_fair_encrypt(string M, string K) {
+string play_fair_encrypt(string M, string K, vector<int> &xPos) {
     string matrix = remove_duplicate_string(K);
     for (char c = 'A'; c <= 'Z' && matrix.size() <= 25; ++c) {
         if (c != 'J' && matrix.find(c) == string::npos) {
@@ -22,6 +22,7 @@ string play_fair_encrypt(string M, string K) {
     for (int i = 0; i < M.size(); i += 2) {
         if (i + 1 == M.size() || M[i] == M[i + 1]) {
             M.insert(i + 1, "X");
+            xPos.push_back(i + 1);
         }
     }
     string C = "";
@@ -44,9 +45,43 @@ string play_fair_encrypt(string M, string K) {
     return C;
 }
 
+string play_fair_decrypt(string C, string K, vector<int> xPos) {
+    string matrix = remove_duplicate_string(K);
+    for (char c = 'A'; c <= 'Z' && matrix.size() <= 25; ++c) {
+        if (c != 'J' && matrix.find(c) == string::npos) {
+            matrix += c;
+        }
+    }
+    string M = "";
+    for (int i = 0; i < C.size(); i += 2) {
+        int firstPos = matrix.find(C[i]), secondPos = matrix.find(C[i + 1]);
+        int firstRow = firstPos / 5, firstCol = firstPos % 5; 
+        int secondRow = secondPos / 5, secondCol = secondPos % 5;
+
+        if (firstRow == secondRow) {
+            M += matrix[firstRow * 5 + (firstCol + 4) % 5];
+            M += matrix[secondRow * 5 + (secondCol + 4) % 5];
+        } else if (firstCol == secondCol) {
+            M += matrix[(firstRow + 4) % 5 * 5 + firstCol];
+            M += matrix[(secondRow + 4) % 5 * 5 + secondCol];
+        } else {
+            M += matrix[firstRow * 5 + secondCol];
+            M += matrix[secondRow * 5 + firstCol];
+        }
+    }
+    for (int i = xPos.size() - 1; i >= 0; --i) {
+        M.erase(xPos[i], 1);
+    }
+    return M;
+}
+
 int main() {
+    vector <int> xPos;
     string M = "SOFARSOGOODSO";
     string K = "EASTORW";
-    cout << play_fair_encrypt(M, K);
+    string C = play_fair_encrypt(M, K, xPos);
+
+    cout << "C = " << C << endl;
+    cout << "M = " << play_fair_decrypt(C, K, xPos);
     return 0;
 }
--- Python/MHCD/PlayFair.py
+++ Python/MHCD/PlayFair.py
@@ -8,9 +8,12 @@ def play_fair_encrypt(M, K):
             matrix += c
             if len(matrix) == 25:
                 break
+    xPos = []
     for i in range(0, len(M), 2):
         if i + 1 == len(M) or M[i] == M[i + 1]:
             M = M[:i + 1] + "X" + M[i + 1:]
+            xPos.append(i + 1)
+
     C = ""
     for i in range(0, len(M), 2):
         first_pos = matrix.find(M[i])
@@ -26,8 +29,36 @@ def play_fair_encrypt(M, K):
         else:
             C += matrix[first_row * 5 + second_col]
             C += matrix[second_row * 5 + first_col]
-    return C
+    return C, xPos
+
+def play_fair_decrypt(C, K, xPos):
+    matrix = remove_duplicate_string(K)
+    for c in "ABCDEFGHIKLMNOPQRSTUVWXYZ":
+        if c not in matrix:
+            matrix += c
+        if len(matrix) == 25:
+            break
+    M = ""
+    for i in range(0, len(C), 2):
+        firstPos = matrix.find(C[i])
+        secondPos = matrix.find(C[i + 1])
+        firstRow, firstCol = firstPos // 5, firstPos % 5
+        secondRow, secondCol = secondPos // 5, secondPos % 5
+        if firstRow == secondRow:
+            M += matrix[firstRow * 5 + (firstCol + 4) % 5]
+            M += matrix[secondRow * 5 + (secondCol + 4) % 5]
+        elif firstCol == secondCol:
+            M += matrix[(firstRow + 4) % 5 * 5 + firstCol]
+            M += matrix[(secondRow + 4) % 5 * 5 + secondCol]
+        else:
+            M += matrix[firstRow * 5 + secondCol]
+            M += matrix[secondRow * 5 + firstCol]
+    for i in reversed(xPos):
+        M = M[:i] + M[i+1:]
+    return M
 
 M = "SOFARSOGOODSO"
 K = "EASTORW"
-print(play_fair_encrypt(M, K))
+C, xPos = play_fair_encrypt(M, K)
+print("C =", C)
+print("M =", play_fair_decrypt(C, K, xPos))
